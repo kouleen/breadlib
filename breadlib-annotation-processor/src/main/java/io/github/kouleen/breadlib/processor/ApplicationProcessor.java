5319c8cda368af95b85702b2d5c835427da23faf
张青 <ex_qing1.zhang@tcl.com>
2023-03-03 21:31:04 +0800

    - update package

--- breadlib-annotation-processor/src/main/java/io/github/kouleen/breadlib/processor/ApplicationProcessor.java
+++ breadlib-annotation-processor/src/main/java/io/github/kouleen/breadlib/processor/ApplicationProcessor.java
@@ -11,13 +11,16 @@ import javax.lang.model.element.Element;
 import javax.lang.model.element.PackageElement;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.util.Elements;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.util.Set;
 
 /**
  * @author zhangqing
  * @since 2023/2/23 12:26
  */
-@SupportedAnnotationTypes("com.kouleen.breadlib.annotation.PluginMain")
+@SupportedAnnotationTypes("io.github.kouleen.breadlib.annotation.Main")
 @SupportedSourceVersion(SourceVersion.RELEASE_8)
 public class ApplicationProcessor extends AbstractProcessor {
 
@@ -28,17 +31,36 @@ public class ApplicationProcessor extends AbstractProcessor {
         Set<? extends Element> elements = roundEnv.getElementsAnnotatedWith(Main.class);
         for (Element element : elements) {
             if (isProcess) {
-                throw new IllegalStateException("There can only be one PluginMain");
+                throw new IllegalStateException("There can only be one plugin Main");
             }
             isProcess = true;
-            System.out.println("##############################");
-            System.out.println("#          BreadLib          #");
-            System.out.println("##############################");
+            File file = new File(new File("/").getAbsolutePath() + "/io.github.kouleen.breadlib.txt");
+            try {
+                if(!file.exists()){
+                    file.createNewFile();
+                }
+            }catch (Exception exception){
+                exception.printStackTrace();
+            }
             Elements elementUtils = processingEnv.getElementUtils();
             PackageElement packageElement = elementUtils.getPackageOf(element);
             // 获取PluginMain注解所在的包名
             String packageName = packageElement.getQualifiedName().toString();
-
+            try(BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
+                System.out.println("##############################");
+                bufferedWriter.write("##############################\n");
+                System.out.println("#          BreadLib          #");
+                bufferedWriter.write("#          BreadLib          #\n");
+                System.out.println("##############################");
+                bufferedWriter.write("##############################\n");
+                bufferedWriter.write("\n");
+                bufferedWriter.write("\n");
+                bufferedWriter.write("version: 1.0.0\n");
+                bufferedWriter.write("author : kouleen\n");
+                bufferedWriter.write("author : kouleen\n");
+            }catch (Exception exception){
+                exception.printStackTrace();
+            }
         }
         return true;
     }
